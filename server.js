c27c5027b98ca8aabb11b820637bcc2b3284fa51
Inzamamul Alam <inzimunna@gmail.com>
2020-03-03 23:44:35 +0600

    dynamic port section

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.info('listening on %d', port);
 });
